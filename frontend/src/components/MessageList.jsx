36597cfae7594f7051a8f2110962baa2d5a04dd0
sunil kumar rao s <sunilkumarraos007@gmail.com>
2026-07-06 14:01:43 +0530

    added basic ux design

--- frontend/src/components/MessageList.jsx
+++ frontend/src/components/MessageList.jsx
@@ -1,13 +1,21 @@
-function MessageList({messages}){
-    return(
-        <div className="message-list">
-            {messages.map((msg,index)=>(
-                <div key={index} className={`message ${msg.role}`}>
-                    <span className="message-text">{msg.text}</span>
-                </div>
-            ))}
+function MessageList({ messages }) {
+  return (
+    <div className="message-list">
+      {messages.map((msg, index) => (
+        <div key={index} className={`message ${msg.role}`}>
+          {msg.text === '...' ? (
+            <div className="loading-dots">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          ) : (
+            <span className="message-text">{msg.text}</span>
+          )}
         </div>
-    )
+      ))}
+    </div>
+  )
 }
 
 export default MessageList
